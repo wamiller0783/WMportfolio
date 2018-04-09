b96222037ccb0fce696dc1994a9749b43c047bf8
William Miller <wamiller0783@gmail.com>
2018-04-08 19:29:11 -0500

    Changed the HTML for the site.

--- WMPortfolio/settings.py
+++ WMPortfolio/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*@s7@ul9tgy91d^zjm+($^m9w9^+#jiq6k1-xeeywl!6@c*&m9'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
