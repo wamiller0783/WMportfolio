e3dee05b25add431419dcfdeb6288a19290779cf
William Miller <wamiller0783@gmail.com>
2018-04-09 11:43:41 -0500

    CSS and functionality update

--- WMPortfolio/settings.py
+++ WMPortfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "*@s7@ul9tgy91d^zjm+($^m9w9^+#jiq6k1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
